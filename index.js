a3c4fbd0c34e8c433168afea1a684251cc597533
mczekala <32793245+mczekala@users.noreply.github.com>
2018-07-08 19:43:56 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,23 +1,3 @@
-// var image = {
-//   name: 'Kotek',
-//   src: 'http://imgur.com/n8OYCzR.png'
-// };
-
-// var GalleryItem = React.createClass({
-//   propTypes: {
-//     image: React.PropTypes.object.isRequired,
-//   },
-
-//   render: function() {
-//     return(
-//       React.createElement('div', {},
-//         React.createElement('h2', {}, this.props.image.name),
-//         React.createElement('img', { src: this.props.image.src })
-//       )
-//     )
-//   },
-// });
-
 var movies = [{
     id: 1,
     title: 'Harry Potter',
