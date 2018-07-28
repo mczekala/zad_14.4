7ba4de8539a233cadbb1de7f81ce6594f71e37e2
Marcin Czekala <marcin_czekala@icloud.com>
2018-07-29 01:43:44 +0200

    add

--- index.js
+++ index.js
@@ -1,3 +1,82 @@
+var MoviesList = React.createClass({
+  state: {
+    movies: [{
+        id: 1,
+        title: 'Harry Potter',
+        desc: 'film o czarodzieju',
+        image: 'hp.jpg'
+      },
+      {
+        id: 2,
+        title: 'Krol Lew',
+        desc: 'Film o krolu sawanny',
+        image: 'kl.jpg'
+      },
+      {
+        id: 3,
+        title: 'Matrix',
+        desc: 'film o apokalipsie',
+        image: 'mx.jpg'
+      },
+      {
+        id: 4,
+        title: 'Wlatca pierscieni',
+        desc: 'film o ratowaniu swiata',
+        image: 'lotr.jpg'
+      }
+    ]
+  },
+  answer: this.state.movies.map(function (movie) {
+    return React.createElement(Movie, {
+      movie: movie,
+      key: movie.id
+    });
+  }),
+  render: function () {
+    return (
+      React.createElement('div', {},
+        React.createElement('h1', {}, 'lista filmow'),
+        React.createElement('ul', {}, answer),
+      )
+    )
+  }
+});
+
+var Movie = React.createClass({
+  render: function () {
+    return (
+      React.createElement('li', {
+          key: this.props.movie.id
+        },
+        React.createElement('img', {
+          src: this.props.movie.image
+        }),
+        React.createElement('MovieTitle', {
+          title: this.props.movie.title
+        }),
+        React.createElement('MovieDescription', {
+          desc: this.props.movie.desc
+        })
+      )
+    )
+  },
+});
+
+var MovieTitle = React.createClass({
+  render: function () {
+    return (
+      React.createElement('h2', {}, this.props.movie.title)
+    )
+  },
+});
+
+var MovieDescription = React.createClass({
+  render: function () {
+    return (
+      React.createElement('p', {}, this.props.movie.desc)
+    )
+  },
+});
 var movies = [{
     id: 1,
     title: 'Harry Potter',
@@ -22,32 +101,6 @@ var movies = [{
     desc: 'film o ratowaniu swiata',
     image: 'lotr.jpg'
   }
-]
-var Movie = React.createClass({
-  propTypes: {
-    movie: React.PropTypes.object.isRequired,
-  },
-
-  render: function() {
-    return(
-      React.createElement('li', { key: this.props.movie.id },
-        React.createElement('h2', {}, this.props.movie.title),
-        React.createElement('p', {}, this.props.movie.desc),
-        React.createElement('img', { src: this.props.movie.image })
-      )
-    )
-  },
-});
-
-var answer = movies.map(function(movie) {
-  return React.createElement(Movie, { movie: movie, key: movie.id });
-})
-
-
-var element =
-  React.createElement('div', {},
-    React.createElement('h1', {}, 'lista filmow'),
-    React.createElement('ul', {}, answer)
-  );
-
-ReactDOM.render(element, document.getElementById('app'));
+];
+var movie = movies[0];
+ReactDOM.render(React.createElement('MoviesList', {}), document.getElementById('app'));
--- script.js
+++
@@ -1,5 +0,0 @@
-var GalleryItem = React.createClass({
-  render: function() {
-    return React.createElement('h2', {}, 'Pierwszy komponent');
-  }
-});
